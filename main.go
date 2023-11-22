c0678e8610e1a37197604ba25c1156e87aae90de
WCatz <waynecataldo@gmail.com>
2023-11-22 09:55:06 -0500

    working websocket

--- main.go
+++ main.go
@@ -1,18 +1,33 @@
+// main.go
+
 package main
 
 import (
 	"encoding/json"
 	"fmt"
-	"github.com/gorilla/mux"
 	"html/template"
-	"io"
+	"log"
 	"net/http"
 	"os/exec"
 	"path/filepath"
 	"sync"
+
+	"github.com/gorilla/mux"
+	"github.com/gorilla/websocket"
 )
 
-type WebhookPayload struct {
+var (
+	upgrader = websocket.Upgrader{
+		ReadBufferSize:  1024,
+		WriteBufferSize: 1024,
+	}
+	clients   = make(map[*websocket.Conn]bool)
+	clientsMu sync.Mutex
+	events    = make(chan SnekEvent)
+	templates *template.Template
+)
+
+type SnekEvent struct {
 	Type      string `json:"type"`
 	Timestamp string `json:"timestamp"`
 	Context   struct {
@@ -28,74 +43,53 @@ type WebhookPayload struct {
 	} `json:"payload"`
 }
 
-var templates *template.Template
-var mu sync.Mutex
-var latestEventData WebhookPayload
-
 func init() {
 	templatesPath := filepath.Join(".", "templates", "*.html")
 	templates = template.Must(template.ParseGlob(templatesPath))
 }
 
-func renderIndex(w http.ResponseWriter, templateName string, data interface{}) {
-	err := templates.ExecuteTemplate(w, templateName, data)
+func handler(w http.ResponseWriter, r *http.Request) {
+	err := templates.ExecuteTemplate(w, "index.html", r.Host)
 	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
-		return
+		log.Fatal(err)
 	}
 }
 
-func renderWebhookData(w http.ResponseWriter, templateName string, data interface{}) {
-	err := templates.ExecuteTemplate(w, templateName, data)
+func wsHandler(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		http.Error(w, "Error rendering template", http.StatusInternalServerError)
+		log.Fatal(err)
 		return
 	}
-}
+	defer func() {
+		clientsMu.Lock()
+		delete(clients, conn)
+		clientsMu.Unlock()
+		conn.Close()
+	}()
 
-func HomeHandler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
+	clientsMu.Lock()
+	clients[conn] = true
+	clientsMu.Unlock()
 
-	// Serve the index.html file with the latest event data
-	renderIndex(w, "index.html", latestEventData)
-}
-
-func WebhookDataHandler(w http.ResponseWriter, r *http.Request) {
-	mu.Lock()
-	defer mu.Unlock()
-
-	// Serve the webhook_data.html file with the latest event data
-	renderWebhookData(w, "webhook_data.html", latestEventData)
-}
-
-func handleWebhook(w http.ResponseWriter, r *http.Request) {
-	if r.Method != http.MethodPost {
-		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
-		return
+	for {
+		select {
+		case snekEvent := <-events:
+			clientsMu.Lock()
+			for client := range clients {
+				// Check if the WebSocket connection is still open
+				if err := client.WriteJSON(snekEvent); err != nil {
+					if websocket.IsCloseError(err, websocket.CloseNormalClosure) || websocket.IsCloseError(err, websocket.CloseGoingAway) {
+						// Connection is closed by the client, remove it from the clients map
+						delete(clients, client)
+					} else {
+						log.Println("Error writing to client:", err)
+					}
+				}
+			}
+			clientsMu.Unlock()
+		}
 	}
-
-	// Read the request body
-	body, err := io.ReadAll(r.Body)
-	if err != nil {
-		http.Error(w, "Error reading request body", http.StatusInternalServerError)
-		return
-	}
-
-	// Unmarshal the JSON payload into a struct
-	var payload WebhookPayload
-	err = json.Unmarshal(body, &payload)
-	if err != nil {
-		http.Error(w, "Error decoding JSON payload", http.StatusBadRequest)
-		return
-	}
-
-	mu.Lock()
-	defer mu.Unlock()
-
-	// Update the latest event data
-	latestEventData = payload
-	renderIndex(w, "index.html", latestEventData)
 }
 
 func startSnek() {
@@ -105,36 +99,56 @@ func startSnek() {
 		"-output", "webhook",
 		"-output-webhook-url", "http://localhost:42069/webhook")
 
-	err := cmd.Start()
+	stdout, err := cmd.StdoutPipe()
 	if err != nil {
-		fmt.Println("Error starting snek:", err)
+		log.Fatal("Error creating stdout pipe:", err)
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		log.Fatal("Error starting snek:", err)
 		return
 	}
 
 	fmt.Println("Snek started successfully. PID:", cmd.Process.Pid)
+
+	decoder := json.NewDecoder(stdout)
+	for {
+		var snekEvent SnekEvent
+		if err := decoder.Decode(&snekEvent); err != nil {
+			log.Println("Error decoding snek output:", err)
+			break
+		}
+		events <- snekEvent
+	}
+}
+
+func handleWebhook(w http.ResponseWriter, r *http.Request) {
+	var snekEvent SnekEvent
+	if err := json.NewDecoder(r.Body).Decode(&snekEvent); err != nil {
+		log.Println("Error decoding webhook data:", err)
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	// Do something with the webhook data if needed
+	fmt.Printf("Received webhook data: %+v\n", snekEvent)
+
+	// Send the event to WebSocket clients
+	clientsMu.Lock()
+	for client := range clients {
+		err := client.WriteJSON(snekEvent)
+		if err != nil {
+			log.Println("Error writing to client:", err)
+		}
+	}
+	clientsMu.Unlock()
+
+	w.WriteHeader(http.StatusOK)
 }
 
 func main() {
-	// Start Snek asynchronously
 	go startSnek()
-
-	// Set up the Gorilla Mux router for the main server on port 8080
-	mainRouter := mux.NewRouter()
-	mainRouter.HandleFunc("/", HomeHandler).Methods("GET") // Handle root URL
-	mainRouter.PathPrefix("/static/").Handler(http.StripPrefix("/static/", http.FileServer(http.Dir("./static/"))))
-	mainRouter.HandleFunc("/webhook_data", WebhookDataHandler).Methods("GET") // New endpoint for webhook_data
-
-	// Start the main HTTP server on port 8080
-	mainPort := 8080
-	mainAddr := fmt.Sprintf(":%v", mainPort)
-	fmt.Printf("HTML server running on port %v...\n", mainPort)
-
-	go func() {
-		err := http.ListenAndServe(mainAddr, mainRouter)
-		if err != nil {
-			fmt.Println("Error starting main HTTP server:", err)
-		}
-	}()
 
 	// Set up the Gorilla Mux router for the webhook server on port 42069
 	webhookRouter := mux.NewRouter()
@@ -145,8 +159,17 @@ func main() {
 	webhookAddr := fmt.Sprintf(":%v", webhookPort)
 	fmt.Printf("Webhook server running on port %v...\n", webhookPort)
 
-	err := http.ListenAndServe(webhookAddr, webhookRouter)
-	if err != nil {
-		fmt.Println("Error starting webhook HTTP server:", err)
-	}
+	go func() {
+		err := http.ListenAndServe(webhookAddr, webhookRouter)
+		if err != nil {
+			fmt.Println("Error starting webhook HTTP server:", err)
+		}
+	}()
+
+	// Start the WebSocket server
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/ws", wsHandler)
+
+	log.Println("Server started on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
