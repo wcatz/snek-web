b19250bebbb3fbed27d8b73cf203c6765da1e50a
WCatz <waynecataldo@gmail.com>
2023-11-22 16:04:15 -0500

    added a pipeline lol

--- main.go
+++ main.go
@@ -6,7 +6,6 @@ import (
 	"html/template"
 	"log"
 	"net/http"
-
 	"path/filepath"
 	"sync"
 
@@ -20,47 +19,121 @@ var (
 	}
 	clients   = make(map[*websocket.Conn]bool)
 	clientsMu sync.Mutex
-//	events    = make(chan SnekEvent)
 	templates *template.Template
 )
+
+type pipeline struct {
+	input  chan interface{}
+	output chan interface{}
+}
+
+func newPipeline() *pipeline {
+	return &pipeline{
+		input:  make(chan interface{}),
+		output: make(chan interface{}),
+	}
+}
+
+func (p *pipeline) send(data interface{}) {
+	p.input <- data
+}
+
+func (p *pipeline) receive() <-chan interface{} {
+	return p.output
+}
+
+func (p *pipeline) start() {
+	go func() {
+		for {
+			data := <-p.input
+			// You can perform any processing on 'data' here
+			// For simplicity, just forward it to the output channel
+			p.output <- data
+
+			// Send the data to all connected clients
+			clientsMu.Lock()
+			for client := range clients {
+				err := client.WriteMessage(websocket.TextMessage, []byte(data.(string)))
+				if err != nil {
+					log.Printf("Error sending message to client: %v", err)
+				}
+			}
+			clientsMu.Unlock()
+		}
+	}()
+}
 
 func init() {
 	templatesPath := filepath.Join(".", "templates", "*.html")
 	templates = template.Must(template.ParseGlob(templatesPath))
 }
 
+func renderError(w http.ResponseWriter, err error, statusCode int) {
+	log.Printf("Error: %v", err)
+	http.Error(w, http.StatusText(statusCode), statusCode)
+}
+
 func handler(w http.ResponseWriter, r *http.Request) {
 	err := templates.ExecuteTemplate(w, "index.html", r.Host)
 	if err != nil {
-		log.Fatal(err)
+		renderError(w, err, http.StatusInternalServerError)
+		return
 	}
+}
+
+var dataPipeline *pipeline
+
+func init() {
+	dataPipeline = newPipeline()
+	dataPipeline.start()
 }
 
 func wsHandler(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
-		log.Fatal(err)
+		renderError(w, err, http.StatusInternalServerError)
 		return
 	}
 	defer func() {
 		clientsMu.Lock()
 		delete(clients, conn)
 		clientsMu.Unlock()
-		conn.Close()
+		closeErr := conn.Close()
+		if closeErr != nil {
+			log.Printf("Error closing connection: %v", closeErr)
+		}
 	}()
 
 	clientsMu.Lock()
 	clients[conn] = true
 	clientsMu.Unlock()
 
+	// Handle WebSocket messages here
+	go handleWebSocketMessages(conn)
+}
+
+func handleWebSocketMessages(conn *websocket.Conn) {
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			log.Printf("Error reading message: %v", err)
+			return
+		}
+
+		// Send the received message through the pipeline
+		dataPipeline.send(string(message))
+	}
 }
 
 func main() {
-
-	// Start the WebSocket server
 	http.HandleFunc("/", handler)
 	http.HandleFunc("/ws", wsHandler)
 
-	log.Println("Server started on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	addr := ":8080"
+	log.Printf("Server started on %s\n", addr)
+
+	err := http.ListenAndServe(addr, nil)
+	if err != nil {
+		log.Fatalf("Error starting server: %v", err)
+	}
 }
