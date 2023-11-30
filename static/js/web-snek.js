1516691fdd0d67968e09e8b9d0ab8714ced3e047
WCatz <waynecataldo@gmail.com>
2023-11-30 06:53:48 -0500

    seperate js, almost working tx view

--- static/js/web-snek.js
+++ static/js/web-snek.js
@@ -1,175 +1,179 @@
-      // Keep track of the previous slot number
-      let prevSlotNumber;
-      // Event handler for incoming messages
-      function handleWebSocketMessage(event) {
-        // Parse the JSON message
-        const blockEvent = JSON.parse(event.data);
+let prevSlotNumber
 
-        // Create a new div element for each message block
-        const newDiv = document.createElement("div");
-        newDiv.classList.add("message-block");
-        newDiv.id = "message-" + new Date().getTime(); // Unique ID based on timestamp
+function copyToClipboard(element, property) {
+  const propertySpan = element;
+  const tempInput = document.createElement("textarea");
+  tempInput.value = propertySpan.innerText;
+  document.body.appendChild(tempInput);
+  tempInput.select();
+  document.execCommand("copy");
+  document.body.removeChild(tempInput);
 
-        // Calculate time difference between consecutive messages based on slot number
-        let timeDifference = "";
-        if (prevSlotNumber !== undefined) {
-          const slotsDiff = blockEvent.context.slotNumber - prevSlotNumber;
+  // Highlight copy
+  propertySpan.classList.add(
+    "text-white",
+    "transition-color",
+    "duration-300"
+  );
+  setTimeout(() => {
+    propertySpan.classList.remove("text-white");
+  }, 500);
+}
 
-          if (slotsDiff < 60) {
-            // If less than 60 seconds, display only seconds
-            timeDifference = `${slotsDiff} Ssecondsss`;
-          } else {
-            // If more than 60 seconds, convert to minutes and seconds
-            const minutes = Math.floor(slotsDiff / 60);
-            const remainingSeconds = slotsDiff % 60;
-            timeDifference = `${minutes} Minutesss ${remainingSeconds} Ssecondssss`;
-          }
-        }
-        // Convert block size to kilobytes
-        const blockBodySize = blockEvent.payload.blockBodySize;
-        const blockBodySizeKB = blockBodySize / 1024;
-
-        // Full block size in kilobytes
-        const fullBlockSize = 90112;
-
-        // Calculate percentage full based on kilobytes
-        const percentageFull = (blockBodySizeKB / (fullBlockSize / 1024)) * 100;
-
-        // Populate the new div with content
-        newDiv.innerHTML = `
-<div class="zoom-in mx-auto max-w-5xl px-4 sm:px-6 lg:px-8 bg-black p-6 rounded-lg shadow-lg">
-  <pre class="whitespace-pre-line text-green-600 mx-auto">
-    <span class="text-blue-400">Type</span><span class="text-white">:</span> ${
-      blockEvent.type
-    }
-    <span class="text-blue-400">Timestamp</span><span class="text-white">:</span> ${
-      blockEvent.timestamp
-    }
-    <span class="text-blue-400">Block Number</span><span class="text-white">:</span> ${
-      blockEvent.context.blockNumber
-    }
-    <span class="text-blue-400">Slot Number</span><span class="text-white">:</span> ${
-      blockEvent.context.slotNumber
-    }
-    <span class="text-blue-400">Block Size</span><span class="text-white">:</span> ${blockBodySizeKB.toFixed(
-      2
-    )} KB, ${percentageFull.toFixed(2)}% full
-    <span class="text-blue-400">Pool</span><span class="text-white">:</span> <span class="whitespace-pre-line text-wrap" id="issuerVkey">${
-      blockEvent.payload.issuerVkey
-    }</span>
-    <span class="text-blue-400">Block Hash</span><span class="text-white">:</span> <span class="whitespace-pre-line text-wrap" id="blockHash">${
-      blockEvent.payload.blockHash
-    }</span>
-    <span class="text-blue-400">Transaction Count</span><span class="text-white">:</span> ${
-      blockEvent.payload.transactionCount
-    }
-  </pre>
-  <div class="flex justify-center gap-1 mb-2">
-    <button onclick="copyToClipboard('${
-      newDiv.id
-    }', 'issuerVkey')" class="copy-button mt-2 border border-green-600 hover:border-green-700 text-blue-400 hover:text-blue-500 py-1 px-2 rounded focus:outline-none focus:border-green-700 active:border-green-900 active:text-blue-600">
-      Copy Pool
-    </button>
-    <button onclick="copyToClipboard('${
-      newDiv.id
-    }', 'blockHash')" class="copy-button mt-2 border border-green-600 hover:border-green-700 text-blue-400 hover:text-blue-500 py-1 px-2 rounded focus:outline-none focus:border-green-700 active:border-green-900 active:text-blue-600">
-      Copy Hash
-    </button>
-  </div>
-</div>
-<div class="text-center py-1 text-white">${timeDifference}</div>
-`;
-
-        // Update the previous slot number for the next message
-        prevSlotNumber = blockEvent.context.slotNumber;
-
-        document
-          .getElementById("messages-container")
-          .insertBefore(
-            newDiv,
-            document.getElementById("messages-container").firstChild
-          );
+// Fetch the current node address on page load
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("/getNodeAddress")
+    .then((response) => response.json())
+    .then((data) => {
+      if (data.nodeAddress) {
+        document.getElementById("nodeAddressInput").value =
+          data.nodeAddress;
       }
+    })
+    .catch((error) => console.error("Error fetching node address:", error));
+});
 
-      // Create a WebSocket connection
-      const socket = new WebSocket("ws://localhost:8080/ws");
+function updateNodeAddress() {
+  var newNodeAddress = document.getElementById("nodeAddressInput").value;
 
-      // Event handler for when the WebSocket connection is established
-      socket.addEventListener("open", (event) => {
-        console.log("WebSocket connection established:", event);
+  fetch("/updateNodeAddress", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newNodeAddress),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Node address updated:", data);
+    })
+    .catch((error) => {
+      console.error("Error updating node address:", error);
+    });
+}
+
+function updateEventType(selectedEventType) {
+  if (selectedEventType) {
+    fetch("/updateEventType", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(selectedEventType),
+    })
+      .then((response) => response.text())
+      .then((data) => {
+        console.log("Event type updated:", data);
+      })
+      .catch((error) => {
+        console.error("Error updating event type:", error);
       });
+  } else {
+    console.error("Selected event type is null or undefined");
+  }
+}
 
-      // Event handler for WebSocket errors
-      socket.addEventListener("error", (event) => {
-        console.error("WebSocket error:", event);
-      });
+document.addEventListener("DOMContentLoaded", function () {
+  const toggleButton = document.getElementById("toggleButton");
+  const closeButton = document.getElementById("closeButton");
+  const offcanvasMenu = document.querySelector(".offcanvas");
+  const toggleMenu = function () {
+    offcanvasMenu.classList.toggle("transform-none");
+  };
 
-      // Event handler for WebSocket closure
-      socket.addEventListener("close", (event) => {
-        console.log("WebSocket connection closed:", event);
-      });
+  // Toggle menu when clicking the toggle button
+  toggleButton.addEventListener("click", toggleMenu);
 
-      // Event handler for incoming messages
-      socket.addEventListener("message", handleWebSocketMessage);
+  // Close menu when clicking the close button
+  closeButton.addEventListener("click", toggleMenu);
 
-      socket.addEventListener("message", function (event) {
-        if (event.data === "refresh") {
-          window.location.reload();
-          return;
-        }
-      });
+  // Close menu when clicking outside the menu or pressing the Escape key
+  document.addEventListener("mousedown", function (event) {
+    if (
+      !offcanvasMenu.contains(event.target) &&
+      event.target !== toggleButton
+    ) {
+      offcanvasMenu.classList.remove("transform-none");
+    }
+  });
 
-      function copyToClipboard(divId, property) {
-        const element = document.getElementById(divId);
-        const propertySpan = element.querySelector(`span#${property}`);
-        const propertyValue = propertySpan.innerText;
+  // Close menu when pressing the Escape key
+  document.addEventListener("keydown", function (event) {
+    if (event.key === "Escape") {
+      offcanvasMenu.classList.remove("transform-none");
+    }
+  });
 
-        const tempInput = document.createElement("textarea");
-        tempInput.value = propertyValue;
-        document.body.appendChild(tempInput);
-        tempInput.select();
-        document.execCommand("copy");
-        document.body.removeChild(tempInput);
+  // Close menu when clicking on the page (outside the menu or toggle button)
+  document.addEventListener("focusin", function (event) {
+    if (
+      !offcanvasMenu.contains(event.target) &&
+      event.target !== toggleButton
+    ) {
+      offcanvasMenu.classList.remove("transform-none");
+    }
+  });
+});
+// When the user scrolls down 2 screen heights, show the button
+window.onscroll = function () {
+  const scrollThreshold = window.innerHeight * 2;
+  const toTopButton = document.getElementById("toTop");
 
-        // Highlight copy
-        propertySpan.classList.add(
-          "text-green-300",
-          "transition-color",
-          "duration-500"
-        );
-        setTimeout(() => {
-          propertySpan.classList.remove("text-green-300");
-        }, 500);
-      }
-      function updateNodeAddress() {
-        var newNodeAddress = document.getElementById("nodeAddressInput").value;
+  if (
+    document.body.scrollTop > scrollThreshold ||
+    document.documentElement.scrollTop > scrollThreshold
+  ) {
+    toTopButton.classList.remove("hidden");
+  } else {
+    toTopButton.classList.add("hidden");
+  }
+};
+// When the user clicks on the button, scroll to the top of the document
+function goToTop() {
+  window.scrollTo({ top: 0, behavior: "smooth" });
+}
 
-        fetch("/updateNodeAddress", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(newNodeAddress),
-        })
-          .then((response) => response.json())
-          .then((data) => {
-            console.log("Node address updated:", data);
-          })
-          .catch((error) => {
-            console.error("Error updating node address:", error);
-          });
-      }
-      // Fetch the current node address on page load
-      document.addEventListener("DOMContentLoaded", function () {
-        fetch("/getNodeAddress")
-          .then((response) => response.json())
-          .then((data) => {
-            if (data.nodeAddress) {
-              document.getElementById("nodeAddressInput").value =
-                data.nodeAddress;
-            }
-          })
-          .catch((error) =>
-            console.error("Error fetching node address:", error)
-          );
-      });
+// Function to calculate time difference based on slot number
+function calculateTimeDifference(currentEvent) {
+let timeDifference = "";
+
+// Check if previous slot number is defined
+if (prevSlotNumber !== undefined) {
+// Determine the slot number based on the event type
+const currentSlotNumber =
+  currentEvent.type === "chainsync.block"
+    ? currentEvent.context.slotNumber
+    : currentEvent.type === "chainsync.rollback"
+    ? currentEvent.payload.slotNumber
+    : currentEvent.context.slotNumber;
+
+// Only calculate time difference if the slot number has increased
+if (currentSlotNumber > prevSlotNumber) {
+  // Calculate time difference based on the slot number of the current event
+  const slotsDiff = Math.abs(currentSlotNumber - prevSlotNumber);
+  timeDifference = formatTimeDifference(slotsDiff);
+}
+}
+
+// Update the previous slot number for the next message
+prevSlotNumber =
+currentEvent.type === "chainsync.block"
+  ? currentEvent.context.slotNumber
+  : currentEvent.type === "chainsync.rollback"
+  ? currentEvent.payload.slotNumber
+  : currentEvent.context.slotNumber;
+
+return timeDifference;
+}
+
+// Function to format time difference in a human-readable format
+function formatTimeDifference(slotsDiff) {
+if (slotsDiff < 60) {
+return `${slotsDiff} seconds`;
+} else {
+const minutes = Math.floor(slotsDiff / 60);
+const remainingSeconds = slotsDiff % 60;
+return `${minutes} minutes ${remainingSeconds} seconds`;
+}
+}
+
